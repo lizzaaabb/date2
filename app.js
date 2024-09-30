0c147e89a84eea0500ac9922e7bdb800213b992d
lizzaaabb <lbogveradze12@gmail.com>
2024-09-30 19:01:21 +0400

    lala

---
+++ app.js
@@ -0,0 +1,9 @@
+const yesBtn = document.getElementById("yes")
+const noBtn = document.getElementById("no")
+
+noBtn.addEventListener("click", ()=>{
+    window.alert("you clicked no")
+})
+yesBtn.addEventListener("click", ()=>{
+    window.alert("you clicked yes")
+})
